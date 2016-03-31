ab345b1e9ef72aa08f21afdb3ebbc979b03b2d3f
gman999 <none@host.tld>
2016-03-31 13:03:24 -0400

    a useful pre-flight activity

--- openbsd/TOOLS/mystuff_distfiles.sh
+++ openbsd/TOOLS/mystuff_distfiles.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
-# get distfiles for all ports in mystuff/
+# get distfiles for all ports in /usr/ports/mystuff
+# perfect for pre-flight boarding
+# need to dump errors for "file not found" "bad checksums" etc.
 
-for dir in /usr/ports/mystuff/*/*; do (cd "$dir" && make fetch-all); done
+for dir in /usr/ports/mystuff/*/*; do (cd "$dir" && make fetch-all && make makesum && make checksum); done
 
 true
